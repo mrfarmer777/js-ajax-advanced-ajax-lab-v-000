f2d987335bbeb963fe6cd7329559db6571a01415
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 14:32:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,15 +7,9 @@ function getRepositories(){
 
 function showRepositories(event, data) {
   const repos = JSON.parse(this.responseText);
-  const repoList = '<ul>'+ repos.map(r => {
-    return (`
-          <li>
-            <h2><a href="${r.html_url}">${r.name}</a></h2>
-            <p>Watchers: ${r.watchers_count}</p>
-            <p>Forks: ${r.forks_count}</p>
-            <p>Issues: ${r.open_issues_count}</p>
-          </li>`
-          )
-  }).join('') + "</ul>"
-  document.getElementById("repositories").innerHTML = repoList
+  
+  const src=document.getElementById("repository-template");
+  const template=Handlebars.compile(src);
+  const result=template(repos);
+  document.getElementById("repositories").innerHTML = result;
 }
