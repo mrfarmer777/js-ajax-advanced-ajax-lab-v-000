2b7194488e144d93e4ba946d823841fce2bceae6
Matt Farmer <mrfarmer777@gmail.com>
2018-07-29 14:36:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+document.addEventListener("DOMContentLoaded",function(even){
+  Handlebars.registerPartial("authorPartial",document.getElementById("author-partial-template").innerHTML);
+});
+
+
 function getRepositories(){
   const req=new XMLHttpRequest();
   req.addEventListener("load",showRepositories);
